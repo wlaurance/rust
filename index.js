f9f73b5ca22ba92dde0179965564858fed831a3d
wlaurance <w.laurance@gmail.com>
2013-03-19 13:00:01 -0400

    Use the correct replace bin

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var fs = require('fs'),
   backend = require('./backend');
 
 var replace = function(params, callback){
-  var p = spawn('./node_modules/.bin/replace', [params.regex, params.value, params.file]);
+  var p = spawn(__dirname + '/node_modules/.bin/replace', [params.regex, params.value, params.file]);
   p.on('exit', function(code){
     callback();
   });
